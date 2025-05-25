b318b4bbfab6bc5f73d83042b006ef054b22e2ca
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2025-05-25 14:07:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Pattern/pattern.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void printDiamond(int n) {
+        // code here
+        for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            cout << " "; 
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j < n; j++) {
+                cout << " "; // Print leading spaces
+            }
+            for (int j = 1; j <= i; j++) {
+                cout << "* "; // Print stars
+            }
+            cout << endl;
+        }
+
+    }
+};
