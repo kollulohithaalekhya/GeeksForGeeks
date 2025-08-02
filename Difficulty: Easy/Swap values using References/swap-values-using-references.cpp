aa2b6b1b2b43c11c6e3b85c316589c6ae54f347e
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2025-08-02 22:23:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap values using References/swap-values-using-references.cpp
@@ -0,0 +1,10 @@
+//  Complete function to swap A and B using reference
+
+void swap(/* Declare two variables a and b 
+            as reference variables here */ int &a, int &b) {
+    // code here
+    int temp = a;
+    a = b;
+    b = temp;
+
+}
