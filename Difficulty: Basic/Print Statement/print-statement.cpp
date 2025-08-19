65b51b6053b71adba22a72195ebf9de1b0482eb3
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2025-08-19 18:26:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Statement/print-statement.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+    // Print S and move cursor to a new line
+    cout<<s<<endl;
+    return 0;
+}
