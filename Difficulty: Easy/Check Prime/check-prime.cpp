7b5c672a4644b188e6fce46265f0176c16569bf8
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2025-08-29 19:43:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check Prime/check-prime.cpp
+++ Difficulty: Easy/Check Prime/check-prime.cpp
@@ -1,19 +1,22 @@
-// User function Template for C++
+#include <iostream>
+using namespace std;
 
-bool prime(int n) {
-
-    // Write your code here
-    if(n==1)
-    {
-        return false;
+bool isPrime(int n) {
+    if (n <= 1) return false; 
+    if (n == 2) return true; 
+    if (n % 2 == 0) return false; 
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
     }
-    for(int i=2;i<n;i++)
-    {
-        if (n%i==0)
-        {
-            return false;
-        }
-    }
-    // returns a boolean value
     return true;
 }
+int main() {
+    int n;
+    cin >> n;
+    if (isPrime(n)) {
+        cout << "True" << endl;
+    } else {
+        cout << "False" << endl;
+    }
+}
+
