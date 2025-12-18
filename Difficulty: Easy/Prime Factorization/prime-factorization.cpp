e0371655097277bd3db06a472059abff16a7dc64
kollulohithaalekhya <147791802+kollulohithaalekhya@users.noreply.github.com>
2025-12-18 20:13:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Factorization/prime-factorization.cpp
@@ -0,0 +1,13 @@
+void printPrimeFactorization(int n) {
+    // code here
+     while(n%2==0){
+        cout<<2<<" ";
+        n/=2;
+    }
+    for(int i=3;i<=n;i+=2){
+        while(n%i==0){
+            cout<<i<<" ";
+            n/=i;
+        }
+    }
+}
